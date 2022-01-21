a0a0a454e51980817c9ebde56c0e39249d458eb6
py-ma <85931136+py-ma@users.noreply.github.com>
2022-01-21 20:05:13 +0300

    Update urls.py

--- main/main/urls.py
+++ main/main/urls.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
+from .linkmaker import link
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
+
+    path('redirect_link', RedirectView.as_view(url=link)),
+
     path('', include('app.urls'))
 
-            ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
