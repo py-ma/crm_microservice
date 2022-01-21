2091c2a5388c5f2eb236c06bf21d421d082fcb4d
py-ma <85931136+py-ma@users.noreply.github.com>
2022-01-21 19:57:46 +0300

    Update admin.py

--- main/app/admin.py
+++ main/app/admin.py
@@ -1,6 +1,2 @@
 from django.contrib import admin
 from .models import ModelsForm
-'''from django.contrib.admin.models import LogEntry
-LogEntry.objects.all().delete()'''
-'''for delete actions in admin_panel'''
-admin.site.register(ModelsForm)
